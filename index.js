55ce500df83e39902cb093fc1f2698d480d8f7e7
Rayshawn Woolard <r.woolard456@gmail.com>
2018-11-13 00:43:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function johnLennonFacts(facts) {
 }
 
 function iLoveTheBeatles(number) {
+var newArray = [];
     do {
-  var newArray = [];
   newArray.push("I love the Beatles!");
   (number++);
 } while (number < 15)
